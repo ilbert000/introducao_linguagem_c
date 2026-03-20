35f30e4958f4846b0245793d6acbfc38d9a10c62
ilbert Dias <ilbertruda0@gmail.com>
2026-03-20 08:32:12 -0300

    sistema de notas versão final

--- main.c
+++ main.c
@@ -3,23 +3,23 @@
 int main() {
 
     float nota1, nota2, nota3;
-    float media;
+    float somatorioDasMedias;
 
     printf("digite a primeira nota: ");
-    scanf("%.0f", &nota1);
+    scanf("%f ", &nota1);
 
     printf("digite a segunda nota: ");
-    scanf("%.0f", &nota2);
+    scanf("%f \n", &nota2);
 
     printf("digite a terceira nota: ");
-    scanf("%.0f", &nota3);
+    scanf("%f \n", &nota3);
 
-    media = (nota1 + nota2 + nota3  /3);
+    somatorioDasMedias = ((nota1 + nota2 + nota3)  /3);
 
-    if (&media >= 7){
-        printf("aluno aprovado com media: %.2f", media);
+    if (somatorioDasMedias < 7){
+        printf("\naluno reprovado com media: %.2f", somatorioDasMedias);
     } else {
-        printf("aluno reprovado com media: %.2f", media);
+        printf("\naluno aprovado com media: %.2f", somatorioDasMedias);
     }
     return 0;
 }
