d555e2edf5fe0037b81e4f95d138414bb13bcaca
ilbert Dias <ilbertruda0@gmail.com>
2026-03-20 07:59:57 -0300

    sistema de media de notas academicas

--- main.c
+++ main.c
@@ -1,6 +1,25 @@
 #include <stdio.h>
 
 int main() {
-    
+
+    float nota1, nota2, nota3;
+    float media;
+
+    printf("digite a primeira nota: ");
+    scanf("%.0f", &nota1);
+
+    printf("digite a segunda nota: ");
+    scanf("%.0f", &nota2);
+
+    printf("digite a terceira nota: ");
+    scanf("%.0f", &nota3);
+
+    media = (nota1 + nota2 + nota3  /3);
+
+    if (&media >= 7){
+        printf("aluno aprovado com media: %.2f", media);
+    } else {
+        printf("aluno reprovado com media: %.2f", media);
+    }
     return 0;
 }
