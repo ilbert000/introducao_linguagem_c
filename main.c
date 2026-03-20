19836d8c0831eff67616c9d77d7f88395f769ecd
ilbert Dias <ilbertruda0@gmail.com>
2026-03-20 09:20:08 -0300

    versã final do projeto de media de notas- com melhorias

--- main.c
+++ main.c
@@ -1,18 +1,34 @@
 #include <stdio.h>
 
 int main() {
+    // variaveis para as notas e o somatorio das medias
 
     float nota1, nota2, nota3;
     float somatorioDasMedias;
 
+    // entrada das notas
+
     printf("digite a primeira nota: ");
-    scanf("%f ", &nota1);
+    while (scanf("%f", &nota1) != 1 || nota1 < 0 || nota1 > 10) {
+        printf("entrada invalida\n");
+        printf("tente novamente\n");
+        printf("digite a primeira nota: ");
+    }
 
     printf("digite a segunda nota: ");
-    scanf("%f \n", &nota2);
-
+    while (scanf("%f", &nota2) != 1 || nota2 < 0 || nota2 > 10) {
+        printf("entrada invalida\n");
+        printf("tente novamente\n");
+        printf("digite a segunda nota: ");
+    }
     printf("digite a terceira nota: ");
-    scanf("%f \n", &nota3);
+    while (scanf("%f", &nota3) != 1 || nota3 < 0 || nota3 > 10) {
+        printf("entrada invalida\n");
+        printf("tente novamente\n");
+        printf("digite a terceira nota: ");
+    }
+
+    // calculo do somatorio das medias
 
     somatorioDasMedias = ((nota1 + nota2 + nota3)  /3);
 
@@ -23,3 +39,5 @@ int main() {
     }
     return 0;
 }
+
+//professor não é IA eu só sei bastante de logica de programação, pelo amor do pai, Deus eu tenho medo, não zera professor
